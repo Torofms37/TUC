89a17d076f96aebc95164c1c1fb9ca03efd0ada9
torofms37 <torofms37@gmail.com>
2025-06-03 23:20:55 -0700

    v-0.1

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
-import './App.css'
 import { ModalGlobal } from './ui/ModalGlobal'
+import './App.css'
+import { ButtonAuth } from './ui/ButtonAuth';
 
 function App() {
 
   return (
-    <div className="App">
+    <div className="app">
+      <ButtonAuth />
       <ModalGlobal />
     </div>
   )
 }
 
-export default App
+export default App;
--- src/ui/ModalGlobal.jsx
+++ src/ui/ModalGlobal.jsx
@@ -1,11 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
+import '../App.css'
+
+const t = {
+  title: '¿Estás seguro?',
+  subtitle: 'Esta acción no se puede deshacer.',
+}
 
 export const ModalGlobal = () => {
+  const [button, setButton] = useState(false);
+
+  //adecuar al momento
+  if (!button) return true
+
   return (
-    <div>
-      <h1>Modal Global</h1>
-      <p>This is a global modal component.</p>
-      <button className='warning text-white' onClick={() => alert('Modal closed!')}>Close Modal</button>
+    <div className='modal-global text-white text-center shadow-2xl'>
+      <h1 className='text-3xl font-bold'>{t.title}</h1>
+      <h3 className='text-2xl'>{t.subtitle}</h3>
+      <div className='flex w-full justify-center m-2 gap-4'>
+        <button className='warning' onClick={() => setButton(true)}>No</button>
+        <button className='alert'>Sí</button>
+      </div>
     </div>
   )
 }
